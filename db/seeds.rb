077255881bb5c6217d059d0f1ff5917b74a6cb2e
Felix Bourassa-Moreau <felix@bourassa-moreau.com>
2023-01-16 21:44:11 -0800

    Handles CRUD operations on pokemons
    WIP CRUD operations on pokemons
    
    WIP CRUD operations on pokemons
    
    Updated tests on pokemons.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'pokemons.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Pokemon.new
+  t.number = row['number']
+  t.name = row['name']
+  t.type1 = row['type1']
+  t.type2 = row['type2']
+  t.total = row['total']
+  t.hp = row['hp']
+  t.attack = row['attack']
+  t.defense = row['defense']
+  t.sp_atk = row['sp_atk']
+  t.sp_def = row['sp_def']
+  t.speed = row['speed']
+  t.generation = row['generation']
+  t.legendary = row['legendary']
+  t.save
+end
